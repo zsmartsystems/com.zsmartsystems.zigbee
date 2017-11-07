d69408106d0d15f7eb14b37d722e1e71d318e571
Chris Jackson <chris@cd-jackson.com>
2017-11-07 14:50:55 +0000

    Add definitions for additional clusters
    Signed-off-by: Chris Jackson <chris@cd-jackson.com> (+1 squashed commit)
    Squashed commits:
    [f9c2cd0] Update zcl_definition.md

---
+++ com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/zcl/clusters/ZclApplianceControlCluster.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2016-2017 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.zcl.clusters;
+
+import com.zsmartsystems.zigbee.ZigBeeEndpoint;
+import com.zsmartsystems.zigbee.ZigBeeNetworkManager;
+import com.zsmartsystems.zigbee.zcl.ZclAttribute;
+import com.zsmartsystems.zigbee.zcl.ZclCluster;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * <b>Appliance Control</b> cluster implementation (<i>Cluster ID 0x001B</i>).
+ * <p>
+ * Code is auto-generated. Modifications may be overwritten!
+ */
+public class ZclApplianceControlCluster extends ZclCluster {
+    // Cluster ID
+    public static final int CLUSTER_ID = 0x001B;
+
+    // Cluster Name
+    public static final String CLUSTER_NAME = "Appliance Control";
+
+    // Attribute initialisation
+    protected Map<Integer, ZclAttribute> initializeAttributes() {
+        Map<Integer, ZclAttribute> attributeMap = new ConcurrentHashMap<Integer, ZclAttribute>(0);
+
+        return attributeMap;
+    }
+
+    /**
+     * Default constructor to create a Appliance Control cluster.
+     *
+     * @param zigbeeManager {@link ZigBeeNetworkManager}
+     * @param zigbeeEndpoint the {@link ZigBeeEndpoint}
+     */
+    public ZclApplianceControlCluster(final ZigBeeNetworkManager zigbeeManager, final ZigBeeEndpoint zigbeeEndpoint) {
+        super(zigbeeManager, zigbeeEndpoint, CLUSTER_ID, CLUSTER_NAME);
+    }
+
+}
