380c47882e4244960ba03a4f81613b425f25ae19
Chris Jackson <chris@cd-jackson.com>
2018-03-31 18:30:04 +0100

    Move classes into internal package (#258)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ com.zsmartsystems.zigbee.dongle.conbee/src/test/java/com/zsmartsystems/zigbee/dongle/conbee/internal/frame/ConBeeDeviceStateRequestTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2016-2018 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.dongle.conbee.internal.frame;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.zsmartsystems.zigbee.dongle.conbee.internal.frame.ConBeeDeviceStateRequest;
+
+/**
+ *
+ * @author Chris Jackson
+ *
+ */
+public class ConBeeDeviceStateRequestTest {
+    @Test
+    public void doRequest() {
+        ConBeeDeviceStateRequest request = new ConBeeDeviceStateRequest();
+        request.setSequence(0);
+        System.out.println(request);
+
+        assertTrue(Arrays.equals(new int[] { 0x07, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0xF1, 0xFF },
+                request.getOutputBuffer()));
+    }
+}
---
+++ com.zsmartsystems.zigbee.dongle.conbee/src/test/java/com/zsmartsystems/zigbee/dongle/conbee/internal/frame/ConBeeReadParameterRequestTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2016-2018 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.dongle.conbee.internal.frame;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.zsmartsystems.zigbee.dongle.conbee.internal.frame.ConBeeNetworkParameter;
+import com.zsmartsystems.zigbee.dongle.conbee.internal.frame.ConBeeReadParameterRequest;
+
+/**
+ *
+ * @author Chris Jackson
+ *
+ */
+public class ConBeeReadParameterRequestTest {
+    @Test
+    public void test() {
+        ConBeeReadParameterRequest request = new ConBeeReadParameterRequest();
+        request.setParameter(ConBeeNetworkParameter.MAC_ADDRESS);
+        request.setSequence(0x15);
+        System.out.println(request);
+
+        assertTrue(Arrays.equals(new int[] { 0x0A, 0x15, 0x00, 0x08, 0x00, 0x01, 0x00, 0x01, 0xD7, 0xff },
+                request.getOutputBuffer()));
+    }
+}
