37912a410ff1322bbeb621e40a3878e935c1b846
Chris Jackson <chris@cd-jackson.com>
2018-04-12 19:22:01 +0100

    Update ZCL Data types (#280)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- com.zsmartsystems.zigbee.autocode/src/main/java/com/zsmartsystems/zigbee/autocode/ZclProtocolDefinitionParser.java
+++ com.zsmartsystems.zigbee.autocode/src/main/java/com/zsmartsystems/zigbee/autocode/ZclProtocolDefinitionParser.java
@@ -475,13 +475,12 @@ public class ZclProtocolDefinitionParser {
         final DataType dataType = new DataType();
         dataType.dataTypeName = columns[2].trim();
         dataType.dataTypeType = attribute.dataType;
+        System.out.println(" Type:::" + attribute.attributeLabel + ":: " + dataType.dataTypeType);
         dataType.dataTypeClass = ZclDataType.getDataTypeMapping().get(attribute.dataType).dataClass;
         if (dataType.dataTypeClass == null) {
             throw new IllegalArgumentException("Type not mapped: " + attribute.dataType);
         }
         attribute.dataTypeClass = dataType.dataTypeClass;
-
-        System.out.println(" Type::::: " + dataType.dataTypeType);
 
         context.dataTypes.put(attribute.dataType, dataType);
         context.cluster.attributes.put(attribute.attributeId, attribute);
