0b03687cd9941957bce00f71870c81d911cae51c
Chris Jackson <chris@cd-jackson.com>
2018-04-20 18:15:29 +0100

    Add MMO Hash Algorithm to generate install keys (#287)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

---
+++ com.zsmartsystems.zigbee/src/test/java/com/zsmartsystems/zigbee/security/MmoHashTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2016-2018 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.security;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Tests for MMO Hash function used for generating link keys from install codes for ZigBee 3 and Smart Energy.
+ *
+ * @author Chris Jackson
+ *
+ */
+public class MmoHashTest {
+
+    @Test
+    public void testString() {
+        MmoHash hash;
+
+        hash = new MmoHash("11223344556677884AF7");
+        assertEquals("4161:8FC0:C83B:0E14:A589:954B:16E3:1466", hash.toString());
+        System.out.println(hash.toString());
+
+        hash = new MmoHash(new int[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x4A, 0xF7 });
+        assertEquals("4161:8FC0:C83B:0E14:A589:954B:16E3:1466", hash.toString());
+        System.out.println(hash.toString());
+
+        hash = new MmoHash("1122 3344 5566 7788 4AF7");
+        System.out.println(hash.toString());
+        assertEquals("4161:8FC0:C83B:0E14:A589:954B:16E3:1466", hash.toString());
+
+        hash = new MmoHash("1122:3344:5566:7788:4AF7");
+        System.out.println(hash.toString());
+        assertEquals("4161:8FC0:C83B:0E14:A589:954B:16E3:1466", hash.toString());
+
+        hash = new MmoHash("83FED3407A939738c552");
+        System.out.println(hash.toString());
+        assertEquals("A833:A774:34F3:BFBD:7A7A:B979:4214:9287", hash.toString());
+    }
+
+    @Test
+    public void testArray() {
+        MmoHash hash;
+
+        hash = new MmoHash(new int[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x4A, 0xF7 });
+        assertEquals("4161:8FC0:C83B:0E14:A589:954B:16E3:1466", hash.toString());
+        System.out.println(hash.toString());
+    }
+}
