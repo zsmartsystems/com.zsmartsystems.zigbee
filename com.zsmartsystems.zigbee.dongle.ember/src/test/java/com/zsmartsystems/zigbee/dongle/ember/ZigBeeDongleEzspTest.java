d21e89842f861b87db5aea3607dec9dd24dd4642
Chris Jackson <chris@cd-jackson.com>
2018-12-19 19:57:03 +0000

    Add Ember test (#477)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- com.zsmartsystems.zigbee.dongle.ember/src/test/java/com/zsmartsystems/zigbee/dongle/ember/ZigBeeDongleEzspTest.java
+++ com.zsmartsystems.zigbee.dongle.ember/src/test/java/com/zsmartsystems/zigbee/dongle/ember/ZigBeeDongleEzspTest.java
@@ -11,6 +11,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
@@ -18,12 +21,16 @@ import org.mockito.Mockito;
 import com.zsmartsystems.zigbee.ExtendedPanId;
 import com.zsmartsystems.zigbee.ZigBeeStatus;
 import com.zsmartsystems.zigbee.dongle.ember.ezsp.EzspFrame;
+import com.zsmartsystems.zigbee.dongle.ember.ezsp.command.EzspStackStatusHandler;
+import com.zsmartsystems.zigbee.dongle.ember.ezsp.structure.EmberStatus;
 import com.zsmartsystems.zigbee.dongle.ember.ezsp.structure.EzspDecisionId;
 import com.zsmartsystems.zigbee.dongle.ember.ezsp.structure.EzspPolicyId;
 import com.zsmartsystems.zigbee.dongle.ember.ezsp.structure.EzspStatus;
 import com.zsmartsystems.zigbee.transport.TransportConfig;
 import com.zsmartsystems.zigbee.transport.TransportConfigOption;
 import com.zsmartsystems.zigbee.transport.TrustCentreJoinMode;
+import com.zsmartsystems.zigbee.transport.ZigBeeTransportReceive;
+import com.zsmartsystems.zigbee.transport.ZigBeeTransportState;
 
 /**
  * Tests for {@link ZigBeeDongleEzsp}
@@ -32,6 +39,17 @@ import com.zsmartsystems.zigbee.transport.TrustCentreJoinMode;
  *
  */
 public class ZigBeeDongleEzspTest {
+    private static int TIMEOUT = 5000;
+
+    protected void setField(Class clazz, Object object, String fieldName, Object newValue) throws Exception {
+        Field field = clazz.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        field.set(object, newValue);
+    }
+
     @Test
     public void setZigBeeExtendedPanId() {
         ZigBeeDongleEzsp dongle = new ZigBeeDongleEzsp(null);
@@ -105,5 +123,35 @@ public class ZigBeeDongleEzspTest {
         dongle.updateTransportConfig(configuration);
         assertEquals(ZigBeeStatus.INVALID_ARGUMENTS,
                 configuration.getResult(TransportConfigOption.TRUST_CENTRE_JOIN_MODE));
+    }
+
+    @Test
+    public void testEzspStackStatusHandler() throws Exception {
+        ZigBeeTransportReceive transport = Mockito.mock(ZigBeeTransportReceive.class);
+
+        final EmberNcp ncp = Mockito.mock(EmberNcp.class);
+        Mockito.when(ncp.getNwkAddress()).thenReturn(1243);
+        ZigBeeDongleEzsp dongle = new ZigBeeDongleEzsp(null) {
+            @Override
+            public EmberNcp getEmberNcp() {
+                return ncp;
+            }
+        };
+        dongle.setZigBeeTransportReceive(transport);
+
+        setField(ZigBeeDongleEzsp.class, dongle, "initialised", true);
+
+        EzspStackStatusHandler response = Mockito.mock(EzspStackStatusHandler.class);
+        Mockito.when(response.getStatus()).thenReturn(EmberStatus.EMBER_NETWORK_UP);
+        dongle.handlePacket(response);
+
+        Mockito.verify(transport, Mockito.timeout(TIMEOUT)).setNetworkState(ZigBeeTransportState.ONLINE);
+        assertEquals(Integer.valueOf(1243), dongle.getNwkAddress());
+
+        response = Mockito.mock(EzspStackStatusHandler.class);
+        Mockito.when(response.getStatus()).thenReturn(EmberStatus.EMBER_NETWORK_DOWN);
+        dongle.handlePacket(response);
+
+        Mockito.verify(transport, Mockito.timeout(TIMEOUT)).setNetworkState(ZigBeeTransportState.OFFLINE);
     }
 }
