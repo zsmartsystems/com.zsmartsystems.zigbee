d4b5e8f2cba8413a481ef6a7db32a6b37f7dcb86
Chris Jackson <chris@cd-jackson.com>
2019-03-10 17:17:56 +0000

    Introduce ZigBeeNetworkState enum to separate transport and network states (#557)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/ZigBeeNetworkState.java
+++ com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/ZigBeeNetworkState.java
@@ -5,29 +5,29 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package com.zsmartsystems.zigbee.transport;
+package com.zsmartsystems.zigbee;
 
 /**
- * An enumeration of the current state of the transport layer.
+ * An enumeration of possible network states
  *
  * @author Chris Jackson
  *
  */
-public enum ZigBeeTransportState {
+public enum ZigBeeNetworkState {
     /**
-     * Transport has not yet been initialised
+     * Network has not yet been initialised
      */
     UNINITIALISED,
     /**
-     * Transport is currently initialising
+     * Network is currently initialising
      */
     INITIALISING,
     /**
-     * Transport is online and able to be used
+     * Network is online and able to be used
      */
     ONLINE,
     /**
-     * Transport is offline and not able to be used
+     * Network is offline and not able to be used
      */
     OFFLINE
 }
