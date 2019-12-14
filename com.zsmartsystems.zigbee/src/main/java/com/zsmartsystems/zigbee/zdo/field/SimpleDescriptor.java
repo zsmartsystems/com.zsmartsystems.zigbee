c876c3cba5461f8b0e05a39da20b9c883b311ee1
Chris Jackson <chris@cd-jackson.com>
2019-12-14 08:43:14 +0000

    Print SimpleDescriptor clusters as hex values (#902)
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/zdo/field/SimpleDescriptor.java
+++ com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/zdo/field/SimpleDescriptor.java
@@ -162,9 +162,37 @@ public class SimpleDescriptor {
 
     @Override
     public String toString() {
-        return "SimpleDescriptor [endpoint=" + endpoint + ", profileId=" + String.format("%04X", profileId)
-                + ", deviceId=" + deviceId + ", deviceVersion=" + deviceVersion + ", inputClusterList="
-                + inputClusterList + ", outputClusterList=" + outputClusterList + "]";
+        StringBuilder builder = new StringBuilder(200);
+        builder.append("SimpleDescriptor [endpoint=");
+        builder.append(endpoint);
+        builder.append(", profileId=");
+        builder.append(String.format("%04X", profileId));
+        builder.append(", deviceId=");
+        builder.append(String.format("%04X", deviceId));
+        builder.append(", deviceVersion=");
+        builder.append(deviceVersion);
+
+        builder.append(", inputClusterList=");
+        appendClusterList(builder, inputClusterList);
+
+        builder.append(", outputClusterList=");
+        appendClusterList(builder, outputClusterList);
+        builder.append(']');
+
+        return builder.toString();
     }
 
+    private void appendClusterList(StringBuilder builder, List<Integer> clusterList) {
+        boolean first = true;
+        builder.append('[');
+        for (int cluster : clusterList) {
+            if (!first) {
+                builder.append(", ");
+            } else {
+                first = false;
+            }
+            builder.append(String.format("%04X", cluster));
+        }
+        builder.append(']');
+    }
 }
