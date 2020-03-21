15a3c63ed21c11c440873433a169e8713bba07e6
Stefan Triller <stefan.triller@telekom.de>
2020-03-21 09:23:07 +0100

    Telegesis scan for quietest channel (#1010)
    Fixes #1009
    
    Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

--- com.zsmartsystems.zigbee.dongle.telegesis.autocode/src/main/java/com/zsmartsystems/zigbee/dongle/telegesis/autocode/ClassGenerator.java
+++ com.zsmartsystems.zigbee.dongle.telegesis.autocode/src/main/java/com/zsmartsystems/zigbee/dongle/telegesis/autocode/ClassGenerator.java
@@ -44,6 +44,9 @@ public abstract class ClassGenerator {
     }
 
     private String toProperCase(String s) {
+        if (s.length() == 0) {
+            return s;
+        }
         return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
     }
 
