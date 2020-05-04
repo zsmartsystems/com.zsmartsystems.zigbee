zsmartsystems/com.zsmartsystems.zigbee

commit 5a91440b8e4774b9e81bd58e57a1da7cfbd87713

EZSP - Add support for custom frames (#1041)

com.zsmartsystems.zigbee.dongle.ember.autocode/src/main/resources/ezsp_protocol.xml
@@ -592,6 +592,61 @@
 			</parameter>
 		</response_parameters>
 	</command>
+
+	<command>
+		<name>customFrame</name>
+		<id>0x47</id>
+		<description>Provides the customer a custom EZSP frame. On the NCP, these frames are only handled if the XNCP library is included. On the NCP side these frames are handled in the emberXNcpIncomingCustomEzspMessageCallback() callback function.</description>
+		<command_parameters>
+			<parameter>
+				<data_type>uint8_t</data_type>
+				<name>payloadLength</name>
+				<description>The length of the custom frame payload (maximum 119 bytes).</description>
+			</parameter>
+			<parameter>
+				<data_type>uint8_t[]</data_type>
+				<name>payload</name>
+				<description>The payload of the custom frame.</description>
+			</parameter>
+		</command_parameters>
+		<response_parameters>
+			<parameter>
+				<data_type>EmberStatus</data_type>
+				<name>status</name>
+				<description>The status returned by the custom command.</description>
+			</parameter>
+			<parameter>
+				<data_type>uint8_t</data_type>
+				<name>replyLength</name>
+				<description>The length of the response.</description>
+			</parameter>
+			<parameter>
+				<data_type>uint8_t[]</data_type>
+				<name>reply</name>
+				<description>The response.</description>
+			</parameter>
+		</response_parameters>
+	</command>
+
+	<command>
+		<name>customFrameHandler</name>
+		<id>0x54</id>
+		<description>A callback indicating a custom EZSP message has been received.</description>
+		<command_parameters>
+		</command_parameters>
+		<response_parameters>
+			<parameter>
+				<data_type>uint8_t</data_type>
+				<name>payloadLength</name>
+				<description>The length of the custom frame payload.</description>
+			</parameter>
+			<parameter>
+				<data_type>uint8_t[]</data_type>
+				<name>payload</name>
+				<description>The payload of the custom frame.</description>
+			</parameter>
+		</response_parameters>
+	</command>
 															
 	<command>
 		<name>getEui64</name>

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/command/EzspCustomFrameHandler.java
@@ -1,0 +1,107 @@
+/**
+ * Copyright (c) 2016-2020 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.dongle.ember.ezsp.command;
+
+import com.zsmartsystems.zigbee.dongle.ember.ezsp.EzspFrameResponse;
+
+/**
+ * Class to implement the Ember EZSP command <b>customFrameHandler</b>.
+ * <p>
+ * A callback indicating a custom EZSP message has been received.
+ * <p>
+ * This class provides methods for processing EZSP commands.
+ * <p>
+ * Note that this code is autogenerated. Manual changes may be overwritten.
+ *
+ * @author Chris Jackson - Initial contribution of Java code generator
+ */
+public class EzspCustomFrameHandler extends EzspFrameResponse {
+    public static final int FRAME_ID = 0x54;
+
+    /**
+     * The length of the custom frame payload.
+     * <p>
+     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
+     */
+    private int payloadLength;
+
+    /**
+     * The payload of the custom frame.
+     * <p>
+     * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
+     */
+    private int[] payload;
+
+    /**
+     * Response and Handler constructor
+     */
+    public EzspCustomFrameHandler(int[] inputBuffer) {
+        // Super creates deserializer and reads header fields
+        super(inputBuffer);
+
+        // Deserialize the fields
+        payloadLength = deserializer.deserializeUInt8();
+        payload = deserializer.deserializeUInt8Array();
+    }
+
+    /**
+     * The length of the custom frame payload.
+     * <p>
+     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
+     *
+     * @return the current payloadLength as {@link int}
+     */
+    public int getPayloadLength() {
+        return payloadLength;
+    }
+
+    /**
+     * The length of the custom frame payload.
+     *
+     * @param payloadLength the payloadLength to set as {@link int}
+     */
+    public void setPayloadLength(int payloadLength) {
+        this.payloadLength = payloadLength;
+    }
+
+    /**
+     * The payload of the custom frame.
+     * <p>
+     * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
+     *
+     * @return the current payload as {@link int[]}
+     */
+    public int[] getPayload() {
+        return payload;
+    }
+
+    /**
+     * The payload of the custom frame.
+     *
+     * @param payload the payload to set as {@link int[]}
+     */
+    public void setPayload(int[] payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder(75);
+        builder.append("EzspCustomFrameHandler [payloadLength=");
+        builder.append(payloadLength);
+        builder.append(", payload=");
+        for (int cnt = 0; cnt < payload.length; cnt++) {
+            if (cnt > 0) {
+                builder.append(' ');
+            }
+            builder.append(payload[cnt]);
+        }
+        builder.append(']');
+        return builder.toString();
+    }
+}

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/command/EzspCustomFrameRequest.java
@@ -1,0 +1,122 @@
+/**
+ * Copyright (c) 2016-2020 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.dongle.ember.ezsp.command;
+
+import com.zsmartsystems.zigbee.dongle.ember.ezsp.EzspFrameRequest;
+import com.zsmartsystems.zigbee.dongle.ember.internal.serializer.EzspSerializer;
+
+/**
+ * Class to implement the Ember EZSP command <b>customFrame</b>.
+ * <p>
+ * Provides the customer a custom EZSP frame. On the NCP, these frames are only handled if the
+ * XNCP library is included. On the NCP side these frames are handled in the
+ * emberXNcpIncomingCustomEzspMessageCallback() callback function.
+ * <p>
+ * This class provides methods for processing EZSP commands.
+ * <p>
+ * Note that this code is autogenerated. Manual changes may be overwritten.
+ *
+ * @author Chris Jackson - Initial contribution of Java code generator
+ */
+public class EzspCustomFrameRequest extends EzspFrameRequest {
+    public static final int FRAME_ID = 0x47;
+
+    /**
+     * The length of the custom frame payload (maximum 119 bytes).
+     * <p>
+     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
+     */
+    private int payloadLength;
+
+    /**
+     * The payload of the custom frame.
+     * <p>
+     * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
+     */
+    private int[] payload;
+
+    /**
+     * Serialiser used to serialise to binary line data
+     */
+    private EzspSerializer serializer;
+
+    /**
+     * Request constructor
+     */
+    public EzspCustomFrameRequest() {
+        frameId = FRAME_ID;
+        serializer = new EzspSerializer();
+    }
+
+    /**
+     * The length of the custom frame payload (maximum 119 bytes).
+     * <p>
+     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
+     *
+     * @return the current payloadLength as {@link int}
+     */
+    public int getPayloadLength() {
+        return payloadLength;
+    }
+
+    /**
+     * The length of the custom frame payload (maximum 119 bytes).
+     *
+     * @param payloadLength the payloadLength to set as {@link int}
+     */
+    public void setPayloadLength(int payloadLength) {
+        this.payloadLength = payloadLength;
+    }
+
+    /**
+     * The payload of the custom frame.
+     * <p>
+     * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
+     *
+     * @return the current payload as {@link int[]}
+     */
+    public int[] getPayload() {
+        return payload;
+    }
+
+    /**
+     * The payload of the custom frame.
+     *
+     * @param payload the payload to set as {@link int[]}
+     */
+    public void setPayload(int[] payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public int[] serialize() {
+        // Serialize the header
+        serializeHeader(serializer);
+
+        // Serialize the fields
+        serializer.serializeUInt8(payloadLength);
+        serializer.serializeUInt8Array(payload);
+        return serializer.getPayload();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder(75);
+        builder.append("EzspCustomFrameRequest [payloadLength=");
+        builder.append(payloadLength);
+        builder.append(", payload=");
+        for (int cnt = 0; cnt < payload.length; cnt++) {
+            if (cnt > 0) {
+                builder.append(' ');
+            }
+            builder.append(payload[cnt]);
+        }
+        builder.append(']');
+        return builder.toString();
+    }
+}

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/command/EzspCustomFrameResponse.java
@@ -1,0 +1,140 @@
+/**
+ * Copyright (c) 2016-2020 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.zsmartsystems.zigbee.dongle.ember.ezsp.command;
+
+import com.zsmartsystems.zigbee.dongle.ember.ezsp.EzspFrameResponse;
+import com.zsmartsystems.zigbee.dongle.ember.ezsp.structure.EmberStatus;
+
+/**
+ * Class to implement the Ember EZSP command <b>customFrame</b>.
+ * <p>
+ * Provides the customer a custom EZSP frame. On the NCP, these frames are only handled if the
+ * XNCP library is included. On the NCP side these frames are handled in the
+ * emberXNcpIncomingCustomEzspMessageCallback() callback function.
+ * <p>
+ * This class provides methods for processing EZSP commands.
+ * <p>
+ * Note that this code is autogenerated. Manual changes may be overwritten.
+ *
+ * @author Chris Jackson - Initial contribution of Java code generator
+ */
+public class EzspCustomFrameResponse extends EzspFrameResponse {
+    public static final int FRAME_ID = 0x47;
+
+    /**
+     * The status returned by the custom command.
+     * <p>
+     * EZSP type is <i>EmberStatus</i> - Java type is {@link EmberStatus}
+     */
+    private EmberStatus status;
+
+    /**
+     * The length of the response.
+     * <p>
+     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
+     */
+    private int replyLength;
+
+    /**
+     * The response.
+     * <p>
+     * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
+     */
+    private int[] reply;
+
+    /**
+     * Response and Handler constructor
+     */
+    public EzspCustomFrameResponse(int[] inputBuffer) {
+        // Super creates deserializer and reads header fields
+        super(inputBuffer);
+
+        // Deserialize the fields
+        status = deserializer.deserializeEmberStatus();
+        replyLength = deserializer.deserializeUInt8();
+        reply = deserializer.deserializeUInt8Array();
+    }
+
+    /**
+     * The status returned by the custom command.
+     * <p>
+     * EZSP type is <i>EmberStatus</i> - Java type is {@link EmberStatus}
+     *
+     * @return the current status as {@link EmberStatus}
+     */
+    public EmberStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * The status returned by the custom command.
+     *
+     * @param status the status to set as {@link EmberStatus}
+     */
+    public void setStatus(EmberStatus status) {
+        this.status = status;
+    }
+
+    /**
+     * The length of the response.
+     * <p>
+     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
+     *
+     * @return the current replyLength as {@link int}
+     */
+    public int getReplyLength() {
+        return replyLength;
+    }
+
+    /**
+     * The length of the response.
+     *
+     * @param replyLength the replyLength to set as {@link int}
+     */
+    public void setReplyLength(int replyLength) {
+        this.replyLength = replyLength;
+    }
+
+    /**
+     * The response.
+     * <p>
+     * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
+     *
+     * @return the current reply as {@link int[]}
+     */
+    public int[] getReply() {
+        return reply;
+    }
+
+    /**
+     * The response.
+     *
+     * @param reply the reply to set as {@link int[]}
+     */
+    public void setReply(int[] reply) {
+        this.reply = reply;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder(101);
+        builder.append("EzspCustomFrameResponse [status=");
+        builder.append(status);
+        builder.append(", replyLength=");
+        builder.append(replyLength);
+        builder.append(", reply=");
+        for (int cnt = 0; cnt < reply.length; cnt++) {
+            if (cnt > 0) {
+                builder.append(' ');
+            }
+            builder.append(reply[cnt]);
+        }
+        builder.append(']');
+        return builder.toString();
+    }
+}

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/EzspFrame.java
@@ -109,6 +109,8 @@ public abstract class EzspFrame {
     protected static final int FRAME_ID_CLEAR_TEMPORARY_DATA_MAYBE_STORE_LINK_KEY283K1 = 0xEE;
     protected static final int FRAME_ID_CLEAR_TRANSIENT_LINK_KEYS = 0x6B;
     protected static final int FRAME_ID_COUNTER_ROLLOVER_HANDLER = 0xF2;
+    protected static final int FRAME_ID_CUSTOM_FRAME = 0x47;
+    protected static final int FRAME_ID_CUSTOM_FRAME_HANDLER = 0x54;
     protected static final int FRAME_ID_D_GP_SEND = 0xC6;
     protected static final int FRAME_ID_D_GP_SENT_HANDLER = 0xC7;
     protected static final int FRAME_ID_DELETE_BINDING = 0x2D;
@@ -258,6 +260,8 @@ public abstract class EzspFrame {
         ezspHandlerMap.put(FRAME_ID_CLEAR_TEMPORARY_DATA_MAYBE_STORE_LINK_KEY283K1, EzspClearTemporaryDataMaybeStoreLinkKey283k1Response.class);
         ezspHandlerMap.put(FRAME_ID_CLEAR_TRANSIENT_LINK_KEYS, EzspClearTransientLinkKeysResponse.class);
         ezspHandlerMap.put(FRAME_ID_COUNTER_ROLLOVER_HANDLER, EzspCounterRolloverHandler.class);
+        ezspHandlerMap.put(FRAME_ID_CUSTOM_FRAME, EzspCustomFrameResponse.class);
+        ezspHandlerMap.put(FRAME_ID_CUSTOM_FRAME_HANDLER, EzspCustomFrameHandler.class);
         ezspHandlerMap.put(FRAME_ID_D_GP_SEND, EzspDGpSendResponse.class);
         ezspHandlerMap.put(FRAME_ID_D_GP_SENT_HANDLER, EzspDGpSentHandler.class);
         ezspHandlerMap.put(FRAME_ID_DELETE_BINDING, EzspDeleteBindingResponse.class);

commit 392d4d6b446f4b2c2439bb7c3dc1790f962144c2

fix compilation issue introduced in PR #1041 (#1042)
* fix compilation issue after generating custom frames commands

* add auto_size element

com.zsmartsystems.zigbee.dongle.ember.autocode/src/main/resources/ezsp_protocol.xml
@@ -602,6 +602,7 @@
 				<data_type>uint8_t</data_type>
 				<name>payloadLength</name>
 				<description>The length of the custom frame payload (maximum 119 bytes).</description>
+				<auto_size>payload</auto_size>
 			</parameter>
 			<parameter>
 				<data_type>uint8_t[]</data_type>
@@ -619,6 +620,7 @@
 				<data_type>uint8_t</data_type>
 				<name>replyLength</name>
 				<description>The length of the response.</description>
+				<auto_size>reply</auto_size>
 			</parameter>
 			<parameter>
 				<data_type>uint8_t[]</data_type>
@@ -639,6 +641,7 @@
 				<data_type>uint8_t</data_type>
 				<name>payloadLength</name>
 				<description>The length of the custom frame payload.</description>
+				<auto_size>payload</auto_size>
 			</parameter>
 			<parameter>
 				<data_type>uint8_t[]</data_type>

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/command/EzspCustomFrameHandler.java
@@ -24,13 +24,6 @@ public class EzspCustomFrameHandler extends EzspFrameResponse {
     public static final int FRAME_ID = 0x54;
 
     /**
-     * The length of the custom frame payload.
-     * <p>
-     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
-     */
-    private int payloadLength;
-
-    /**
      * The payload of the custom frame.
      * <p>
      * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
@@ -45,28 +38,8 @@ public class EzspCustomFrameHandler extends EzspFrameResponse {
         super(inputBuffer);
 
         // Deserialize the fields
-        payloadLength = deserializer.deserializeUInt8();
-        payload = deserializer.deserializeUInt8Array();
-    }
-
-    /**
-     * The length of the custom frame payload.
-     * <p>
-     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
-     *
-     * @return the current payloadLength as {@link int}
-     */
-    public int getPayloadLength() {
-        return payloadLength;
-    }
-
-    /**
-     * The length of the custom frame payload.
-     *
-     * @param payloadLength the payloadLength to set as {@link int}
-     */
-    public void setPayloadLength(int payloadLength) {
-        this.payloadLength = payloadLength;
+        int payloadLength = deserializer.deserializeUInt8();
+        payload= deserializer.deserializeUInt8Array(payloadLength);
     }
 
     /**
@@ -91,9 +64,9 @@ public class EzspCustomFrameHandler extends EzspFrameResponse {
 
     @Override
     public String toString() {
-        final StringBuilder builder = new StringBuilder(75);
-        builder.append("EzspCustomFrameHandler [payloadLength=");
-        builder.append(payloadLength);
+        final StringBuilder builder = new StringBuilder(100);
+        builder.append("EzspCustomFrameHandler [networkId=");
+        builder.append(networkId);
         builder.append(", payload=");
         for (int cnt = 0; cnt < payload.length; cnt++) {
             if (cnt > 0) {

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/command/EzspCustomFrameRequest.java
@@ -27,13 +27,6 @@ public class EzspCustomFrameRequest extends EzspFrameRequest {
     public static final int FRAME_ID = 0x47;
 
     /**
-     * The length of the custom frame payload (maximum 119 bytes).
-     * <p>
-     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
-     */
-    private int payloadLength;
-
-    /**
      * The payload of the custom frame.
      * <p>
      * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
@@ -51,26 +44,6 @@ public class EzspCustomFrameRequest extends EzspFrameRequest {
     public EzspCustomFrameRequest() {
         frameId = FRAME_ID;
         serializer = new EzspSerializer();
-    }
-
-    /**
-     * The length of the custom frame payload (maximum 119 bytes).
-     * <p>
-     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
-     *
-     * @return the current payloadLength as {@link int}
-     */
-    public int getPayloadLength() {
-        return payloadLength;
-    }
-
-    /**
-     * The length of the custom frame payload (maximum 119 bytes).
-     *
-     * @param payloadLength the payloadLength to set as {@link int}
-     */
-    public void setPayloadLength(int payloadLength) {
-        this.payloadLength = payloadLength;
     }
 
     /**
@@ -99,16 +72,16 @@ public class EzspCustomFrameRequest extends EzspFrameRequest {
         serializeHeader(serializer);
 
         // Serialize the fields
-        serializer.serializeUInt8(payloadLength);
+        serializer.serializeUInt8(payload.length);
         serializer.serializeUInt8Array(payload);
         return serializer.getPayload();
     }
 
     @Override
     public String toString() {
-        final StringBuilder builder = new StringBuilder(75);
-        builder.append("EzspCustomFrameRequest [payloadLength=");
-        builder.append(payloadLength);
+        final StringBuilder builder = new StringBuilder(100);
+        builder.append("EzspCustomFrameRequest [networkId=");
+        builder.append(networkId);
         builder.append(", payload=");
         for (int cnt = 0; cnt < payload.length; cnt++) {
             if (cnt > 0) {

com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/command/EzspCustomFrameResponse.java
@@ -34,13 +34,6 @@ public class EzspCustomFrameResponse extends EzspFrameResponse {
     private EmberStatus status;
 
     /**
-     * The length of the response.
-     * <p>
-     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
-     */
-    private int replyLength;
-
-    /**
      * The response.
      * <p>
      * EZSP type is <i>uint8_t[]</i> - Java type is {@link int[]}
@@ -56,8 +49,8 @@ public class EzspCustomFrameResponse extends EzspFrameResponse {
 
         // Deserialize the fields
         status = deserializer.deserializeEmberStatus();
-        replyLength = deserializer.deserializeUInt8();
-        reply = deserializer.deserializeUInt8Array();
+        int replyLength = deserializer.deserializeUInt8();
+        reply= deserializer.deserializeUInt8Array(replyLength);
     }
 
     /**
@@ -78,26 +71,6 @@ public class EzspCustomFrameResponse extends EzspFrameResponse {
      */
     public void setStatus(EmberStatus status) {
         this.status = status;
-    }
-
-    /**
-     * The length of the response.
-     * <p>
-     * EZSP type is <i>uint8_t</i> - Java type is {@link int}
-     *
-     * @return the current replyLength as {@link int}
-     */
-    public int getReplyLength() {
-        return replyLength;
-    }
-
-    /**
-     * The length of the response.
-     *
-     * @param replyLength the replyLength to set as {@link int}
-     */
-    public void setReplyLength(int replyLength) {
-        this.replyLength = replyLength;
     }
 
     /**
@@ -122,11 +95,11 @@ public class EzspCustomFrameResponse extends EzspFrameResponse {
 
     @Override
     public String toString() {
-        final StringBuilder builder = new StringBuilder(101);
-        builder.append("EzspCustomFrameResponse [status=");
+        final StringBuilder builder = new StringBuilder(126);
+        builder.append("EzspCustomFrameResponse [networkId=");
+        builder.append(networkId);
+        builder.append(", status=");
         builder.append(status);
-        builder.append(", replyLength=");
-        builder.append(replyLength);
         builder.append(", reply=");
         for (int cnt = 0; cnt < reply.length; cnt++) {
             if (cnt > 0) {