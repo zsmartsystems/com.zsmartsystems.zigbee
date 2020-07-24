zsmartsystems/com.zsmartsystems.zigbee

commit cc83dea2e8b0750237a264a409eb8a229c9e7f61

Add public ZclCluster.sendCommand(ZclGeneralCommand) (#1090)
Signed-off-by: Chris Jackson <chris@cd-jackson.com>

com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/zcl/ZclCluster.java
@@ -60,6 +60,7 @@ import com.zsmartsystems.zigbee.zcl.clusters.general.WriteAttributesResponse;
 import com.zsmartsystems.zigbee.zcl.clusters.general.WriteAttributesStructuredCommand;
 import com.zsmartsystems.zigbee.zcl.clusters.general.WriteAttributesStructuredResponse;
 import com.zsmartsystems.zigbee.zcl.clusters.general.WriteAttributesUndividedCommand;
+import com.zsmartsystems.zigbee.zcl.clusters.general.ZclGeneralCommand;
 import com.zsmartsystems.zigbee.zcl.field.AttributeInformation;
 import com.zsmartsystems.zigbee.zcl.field.AttributeRecord;
 import com.zsmartsystems.zigbee.zcl.field.AttributeReport;
@@ -290,6 +291,17 @@ public abstract class ZclCluster {
         command.setApsSecurity(apsSecurityRequired);
 
         return zigbeeEndpoint.sendTransaction(command, new ZclTransactionMatcher());
+    }
+
+    /**
+     * Sends a {@link ZclGeneralCommand} and returns the {@link Future} to the result which will complete when the
+     * remote device response is received, or the request times out.
+     *
+     * @param command the {@link ZclGeneralCommand} to send
+     * @return the command result future
+     */
+    public Future<CommandResult> sendCommand(ZclGeneralCommand command) {
+        return sendCommand((ZclCommand) command);
     }
 
     /**
@@ -612,7 +624,8 @@ public abstract class ZclCluster {
         AttributeRecord record = new AttributeRecord();
         record.setAttributeIdentifier(attributeId);
         record.setDirection(0);
-        final ReadReportingConfigurationCommand command = new ReadReportingConfigurationCommand(Collections.singletonList(record));
+        final ReadReportingConfigurationCommand command = new ReadReportingConfigurationCommand(
+                Collections.singletonList(record));
         command.setClusterId(clusterId);
         command.setDestinationAddress(zigbeeEndpoint.getEndpointAddress());
 
@@ -774,7 +787,8 @@ public abstract class ZclCluster {
      * @return Command future
      */
     public Future<CommandResult> bind(IeeeAddress address, int endpointId) {
-        final BindRequest command = new BindRequest(zigbeeEndpoint.getIeeeAddress(), zigbeeEndpoint.getEndpointId(), clusterId, 3, address, endpointId);
+        final BindRequest command = new BindRequest(zigbeeEndpoint.getIeeeAddress(), zigbeeEndpoint.getEndpointId(),
+                clusterId, 3, address, endpointId);
         command.setDestinationAddress(new ZigBeeEndpointAddress(zigbeeEndpoint.getEndpointAddress().getAddress()));
         // The transaction is not sent to the Endpoint of this cluster, but to the ZDO endpoint 0 directly.
         return zigbeeEndpoint.getParentNode().sendTransaction(command, command);
@@ -788,7 +802,8 @@ public abstract class ZclCluster {
      * @return Command future
      */
     public Future<CommandResult> unbind(IeeeAddress address, int endpointId) {
-        final UnbindRequest command = new UnbindRequest(zigbeeEndpoint.getIeeeAddress(), zigbeeEndpoint.getEndpointId(), clusterId, 3, address, endpointId);
+        final UnbindRequest command = new UnbindRequest(zigbeeEndpoint.getIeeeAddress(), zigbeeEndpoint.getEndpointId(),
+                clusterId, 3, address, endpointId);
         command.setDestinationAddress(new ZigBeeEndpointAddress(zigbeeEndpoint.getEndpointAddress().getAddress()));
         // The transaction is not sent to the Endpoint of this cluster, but to the ZDO endpoint 0 directly.
         return zigbeeEndpoint.getParentNode().sendTransaction(command, command);
@@ -1329,7 +1344,8 @@ public abstract class ZclCluster {
             }
         }
 
-        DiscoverAttributesResponse response = new DiscoverAttributesResponse(attributeInformation.size() == getLocalAttributes().size(), attributeInformation);
+        DiscoverAttributesResponse response = new DiscoverAttributesResponse(
+                attributeInformation.size() == getLocalAttributes().size(), attributeInformation);
         sendResponse(command, response);
     }
 
@@ -1895,7 +1911,7 @@ public abstract class ZclCluster {
         if (command.isDisableDefaultResponse()) {
             return null;
         }
-        DefaultResponse defaultResponse = new DefaultResponse(command.getCommandId() ,status);
+        DefaultResponse defaultResponse = new DefaultResponse(command.getCommandId(), status);
         defaultResponse.setTransactionId(command.getTransactionId());
         defaultResponse.setDestinationAddress(command.getDestinationAddress());
         defaultResponse.setClusterId(command.getClusterId());

commit b093fe81b97a1e15238971103401b5b137aa45ef

Fix compilation errors introduced in #1090
Fixes #1092

Signed-off-by: Stefan Triller <stefan.triller@telekom.de>

com.zsmartsystems.zigbee/src/test/java/com/zsmartsystems/zigbee/app/seclient/ZclKeyEstablishmentClientTest.java
@@ -7,9 +7,7 @@
  */
 package com.zsmartsystems.zigbee.app.seclient;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -112,7 +110,8 @@ public class ZclKeyEstablishmentClientTest {
         command.setCommandDirection(ZclCommandDirection.SERVER_TO_CLIENT);
         assertTrue(keClient.commandReceived(command));
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // State gets reset back to UNINITIALISED after the FAILURE
         Mockito.verify(seClient, Mockito.timeout(TIMEOUT)).keyEstablishmentCallback(ZigBeeStatus.FAILURE, 0);
@@ -151,8 +150,7 @@ public class ZclKeyEstablishmentClientTest {
         Mockito.when(cbkeExchange.getCertificate()).thenReturn(local);
         Mockito.when(cbkeExchange.getCbkeEphemeralData()).thenReturn(local);
 
-        InitiateKeyEstablishmentResponse command = new InitiateKeyEstablishmentResponse(
-                1, 0, 0, identity);
+        InitiateKeyEstablishmentResponse command = new InitiateKeyEstablishmentResponse(1, 0, 0, identity);
         command.setCommandDirection(ZclCommandDirection.SERVER_TO_CLIENT);
         assertTrue(keClient.commandReceived(command));
 
@@ -191,7 +189,8 @@ public class ZclKeyEstablishmentClientTest {
         command.setCommandDirection(ZclCommandDirection.SERVER_TO_CLIENT);
         assertTrue(keClient.commandReceived(command));
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // State gets reset back to UNINITIALISED after the FAILURE
         Mockito.verify(seClient, Mockito.timeout(TIMEOUT)).keyEstablishmentCallback(ZigBeeStatus.FAILURE, 0);
@@ -271,12 +270,13 @@ public class ZclKeyEstablishmentClientTest {
 
         ZigBeeCbkeCertificate remoteCert = new CerticomCbkeCertificate(
                 "CAPubKey:0200fde8a7f3d1084224962a4e7c54e69ac3f04da6b8DeviceImplicitCert:0200caa15b4beede65c3139a5c3bc40c9ad153854a270022a300001731f354455354534543410109108301234567890aPrivateKeyReconstructionData:019fcc486fc46980ab4a612725b36f005edff075feDevicePublicKey:020366d312a0abf55654ead3e1624c31faed89c3bb20");
-        InitiateKeyEstablishmentResponse command = new InitiateKeyEstablishmentResponse(
-                1, 22, 33, new ByteArray(remoteCert.getCertificate()));
+        InitiateKeyEstablishmentResponse command = new InitiateKeyEstablishmentResponse(1, 22, 33,
+                new ByteArray(remoteCert.getCertificate()));
         command.setCommandDirection(ZclCommandDirection.SERVER_TO_CLIENT);
         assertTrue(keClient.commandReceived(command));
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // State gets reset back to UNINITIALISED after the FAILURE
         Mockito.verify(seClient, Mockito.timeout(TIMEOUT)).keyEstablishmentCallback(ZigBeeStatus.FAILURE, 0);

com.zsmartsystems.zigbee/src/test/java/com/zsmartsystems/zigbee/app/seclient/ZclKeyEstablishmentServerTest.java
@@ -7,10 +7,7 @@
  */
 package com.zsmartsystems.zigbee.app.seclient;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -81,19 +78,21 @@ public class ZclKeyEstablishmentServerTest {
 
         ZigBeeCbkeCertificate remoteCert = new CerticomCbkeCertificate(
                 "CAPubKey:0200fde8a7f3d1084224962a4e7c54e69ac3f04da6b8DeviceImplicitCert:0200caa15b4beede65c3139a5c3bc40c9ad153854a270022a300001731f354455354534543410109108301234567890aPrivateKeyReconstructionData:019fcc486fc46980ab4a612725b36f005edff075feDevicePublicKey:020366d312a0abf55654ead3e1624c31faed89c3bb20");
-        InitiateKeyEstablishmentRequestCommand initiateCommand = new InitiateKeyEstablishmentRequestCommand(
-                1, 22, 33, new ByteArray(remoteCert.getCertificate()));
+        InitiateKeyEstablishmentRequestCommand initiateCommand = new InitiateKeyEstablishmentRequestCommand(1, 22, 33,
+                new ByteArray(remoteCert.getCertificate()));
         initiateCommand.setCommandDirection(ZclCommandDirection.CLIENT_TO_SERVER);
         assertTrue(keServer.commandReceived(initiateCommand));
 
         Mockito.verify(cbkeExchange, Mockito.timeout(TIMEOUT)).getCertificate();
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // Send the command again - this time we're in the wrong state so we get an error
         assertTrue(keServer.commandReceived(initiateCommand));
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(2)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(2))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // State gets reset back to UNINITIALISED after the FAILURE
         assertEquals(KeyEstablishmentState.UNINITIALISED,
@@ -148,12 +147,13 @@ public class ZclKeyEstablishmentServerTest {
 
         ZigBeeCbkeCertificate remoteCert = new CerticomCbkeCertificate(
                 "CAPubKey:0200fde8a7f3d1084224962a4e7c54e69ac3f04da6b8DeviceImplicitCert:0200caa15b4beede65c3139a5c3bc40c9ad153854a270022a300001731f354455354534543410109108301234567890aPrivateKeyReconstructionData:019fcc486fc46980ab4a612725b36f005edff075feDevicePublicKey:020366d312a0abf55654ead3e1624c31faed89c3bb20");
-        InitiateKeyEstablishmentRequestCommand initiateCommand = new InitiateKeyEstablishmentRequestCommand(
-                1, 22, 33, new ByteArray(remoteCert.getCertificate()));
+        InitiateKeyEstablishmentRequestCommand initiateCommand = new InitiateKeyEstablishmentRequestCommand(1, 22, 33,
+                new ByteArray(remoteCert.getCertificate()));
         initiateCommand.setCommandDirection(ZclCommandDirection.CLIENT_TO_SERVER);
         assertTrue(keServer.commandReceived(initiateCommand));
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // State gets reset back to UNINITIALISED after the FAILURE
         assertEquals(KeyEstablishmentState.UNINITIALISED,
@@ -190,7 +190,8 @@ public class ZclKeyEstablishmentServerTest {
         assertTrue(keServer.commandReceived(command));
 
         // State is not initialised
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         Mockito.verify(cbkeExchange, Mockito.timeout(TIMEOUT).times(1)).completeKeyExchange(false);
 
@@ -229,7 +230,8 @@ public class ZclKeyEstablishmentServerTest {
         assertTrue(keServer.commandReceived(command));
 
         // State is not initialised
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         Mockito.verify(cbkeExchange, Mockito.timeout(TIMEOUT).times(1)).completeKeyExchange(false);
 
@@ -278,7 +280,8 @@ public class ZclKeyEstablishmentServerTest {
         command.setCommandDirection(ZclCommandDirection.CLIENT_TO_SERVER);
         assertTrue(keServer.commandReceived(command));
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         // State gets reset back to UNINITIALISED after the FAILURE
         assertEquals(KeyEstablishmentState.UNINITIALISED,
@@ -379,13 +382,14 @@ public class ZclKeyEstablishmentServerTest {
 
         ZigBeeCbkeCertificate remoteCert = new CerticomCbkeCertificate(
                 "CAPubKey:0200fde8a7f3d1084224962a4e7c54e69ac3f04da6b8DeviceImplicitCert:0200caa15b4beede65c3139a5c3bc40c9ad153854a270022a300001731f354455354534543410109108301234567890aPrivateKeyReconstructionData:019fcc486fc46980ab4a612725b36f005edff075feDevicePublicKey:020366d312a0abf55654ead3e1624c31faed89c3bb20");
-        InitiateKeyEstablishmentRequestCommand initiateCommand = new InitiateKeyEstablishmentRequestCommand(
-                1, 22, 33, new ByteArray(remoteCert.getCertificate()));
+        InitiateKeyEstablishmentRequestCommand initiateCommand = new InitiateKeyEstablishmentRequestCommand(1, 22, 33,
+                new ByteArray(remoteCert.getCertificate()));
         assertTrue(keServer.commandReceived(initiateCommand));
 
         Mockito.verify(cbkeExchange, Mockito.timeout(TIMEOUT)).getCertificate();
 
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(1))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         assertEquals(ZigBeeCryptoSuites.ECC_163K1, keServer.getCryptoSuite());
 
@@ -393,14 +397,16 @@ public class ZclKeyEstablishmentServerTest {
         assertTrue(keServer.commandReceived(dataRequest));
 
         Mockito.verify(cbkeExchange, Mockito.timeout(TIMEOUT)).getCbkeEphemeralData();
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(2)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(2))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         ConfirmKeyDataRequestCommand confirmKey = new ConfirmKeyDataRequestCommand(
                 secureMessageAuthenticationCodeByteArray);
         assertTrue(keServer.commandReceived(confirmKey));
 
         Mockito.verify(cbkeExchange, Mockito.timeout(TIMEOUT)).getResponderMac();
-        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(3)).sendCommand(ArgumentMatchers.any());
+        Mockito.verify(keCluster, Mockito.timeout(TIMEOUT).times(3))
+                .sendCommand((ZclKeyEstablishmentCommand) ArgumentMatchers.any());
 
         keServer.shutdown();
         Mockito.verify(keCluster, Mockito.times(1)).removeCommandListener(keServer);
@@ -415,13 +421,12 @@ public class ZclKeyEstablishmentServerTest {
         assertEquals(Integer.valueOf(55), initiateKeyEstablishmentResponse.getConfirmKeyGenerateTime());
         assertEquals(certificateByteArray, initiateKeyEstablishmentResponse.getIdentity());
 
-        EphemeralDataResponse ephemeralDataResponse = (EphemeralDataResponse) commandArgumentCaptor
-                .getAllValues().get(1);
+        EphemeralDataResponse ephemeralDataResponse = (EphemeralDataResponse) commandArgumentCaptor.getAllValues()
+                .get(1);
         System.out.println(ephemeralDataResponse);
         assertEquals(ephemeralDataByteArray, ephemeralDataResponse.getEphemeralData());
 
-        ConfirmKeyResponse confirmKeyResponse = (ConfirmKeyResponse) commandArgumentCaptor
-                .getAllValues().get(2);
+        ConfirmKeyResponse confirmKeyResponse = (ConfirmKeyResponse) commandArgumentCaptor.getAllValues().get(2);
         System.out.println(confirmKeyResponse);
         assertEquals(secureMessageAuthenticationCodeByteArray, confirmKeyResponse.getSecureMessageAuthenticationCode());
 