4857c507c6d0c965a1511740a1f2249215f71246
Mickael <mickaelmarrache@gmail.com>
2022-03-18 11:26:54 +0200

    format network address as hexadecimal in NeighborTable

--- com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/zdo/field/NeighborTable.java
+++ com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/zdo/field/NeighborTable.java
@@ -265,7 +265,7 @@ public class NeighborTable {
     @Override
     public String toString() {
         return "NeighborTable [extendedPanId=" + extendedPanId + ", extendedAddress=" + extendedAddress
-                + ", networkAddress=" + networkAddress + ", deviceType=" + deviceType + ", rxOnWhenIdle=" + rxOnWhenIdle
+                + ", networkAddress=" + String.format("%04X", networkAddress) + ", deviceType=" + deviceType + ", rxOnWhenIdle=" + rxOnWhenIdle
                 + ", relationship=" + relationship + ", permitJoining=" + permitJoining + ", depth=" + depth + ", lqi="
                 + lqi + "]";
     }
