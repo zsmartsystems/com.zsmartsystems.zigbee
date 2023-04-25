0b778ce858a30565996ec288d485b6db79bad8a0
Victor Toni <victor.toni@gmail.com>
2023-04-25 15:35:48 +0200

    Fix wrong generic type on Comparable for ZigBeeGroup

--- com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/groups/ZigBeeGroup.java
+++ com.zsmartsystems.zigbee/src/main/java/com/zsmartsystems/zigbee/groups/ZigBeeGroup.java
@@ -38,7 +38,7 @@ import com.zsmartsystems.zigbee.zcl.clusters.groups.RemoveGroupResponse;
  *
  * @author Chris Jackson
  */
-public class ZigBeeGroup implements Comparable<Object> {
+public class ZigBeeGroup implements Comparable<ZigBeeGroup> {
     static final int MAX_LABEL_LENGTH = 16;
 
     static final int GROUP_ID_MIN = 0x0000;
@@ -365,7 +365,7 @@ public class ZigBeeGroup implements Comparable<Object> {
     }
 
     @Override
-    public int compareTo(Object that) {
+    public int compareTo(ZigBeeGroup that) {
         if (this == that) {
             return 0;
         }
