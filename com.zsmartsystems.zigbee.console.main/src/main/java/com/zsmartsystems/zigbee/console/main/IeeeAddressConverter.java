0a91189fca06b0baf97f4347ae12f199c527ed1f
Victor Toni <ViToni@users.noreply.github.com>
2023-05-28 11:28:22 +0200

    Fix warnings for rawtypes (#1398)

--- com.zsmartsystems.zigbee.console.main/src/main/java/com/zsmartsystems/zigbee/console/main/IeeeAddressConverter.java
+++ com.zsmartsystems.zigbee.console.main/src/main/java/com/zsmartsystems/zigbee/console/main/IeeeAddressConverter.java
@@ -22,7 +22,7 @@ import com.zsmartsystems.zigbee.IeeeAddress;
  */
 public class IeeeAddressConverter implements Converter {
     @Override
-    public boolean canConvert(Class clazz) {
+    public boolean canConvert(@SuppressWarnings("rawtypes") Class clazz) {
         return clazz.equals(IeeeAddress.class);
     }
 
