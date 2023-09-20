dbdd38a3e2ae6c2e65280226ad832f8988cc85cf
Mickael Marrache <mickaelmarrache@gmail.com>
2023-09-20 16:56:36 +0300

    add help + repeat argument

--- com.zsmartsystems.zigbee.console/src/main/java/com/zsmartsystems/zigbee/console/ZigBeeConsoleSpamReadCommand.java
+++ com.zsmartsystems.zigbee.console/src/main/java/com/zsmartsystems/zigbee/console/ZigBeeConsoleSpamReadCommand.java
@@ -29,12 +29,12 @@ public class ZigBeeConsoleSpamReadCommand extends ZigBeeConsoleAbstractCommand {
 
     @Override
     public String getDescription() {
-        return "Spam read.";
+        return "Read the specified attribute REPEAT (argument value defaults to 10) times sequentially.";
     }
 
     @Override
     public String getSyntax() {
-        return "ENDPOINT CLUSTER ATTRIBUTE";
+        return "ENDPOINT CLUSTER ATTRIBUTE [REPEAT=x]";
     }
 
     @Override
@@ -45,7 +45,7 @@ public class ZigBeeConsoleSpamReadCommand extends ZigBeeConsoleAbstractCommand {
     @Override
     public void process(ZigBeeNetworkManager networkManager, String[] args, PrintStream out)
             throws IllegalArgumentException, InterruptedException, ExecutionException {
-        if (args.length != 4) {
+        if (args.length < 4) {
             throw new IllegalArgumentException("Invalid number of arguments");
         }
 
@@ -53,12 +53,13 @@ public class ZigBeeConsoleSpamReadCommand extends ZigBeeConsoleAbstractCommand {
         ZclCluster cluster = getCluster(endpoint, args[2]);
         Integer attributeId = parseAttribute(args[3]);
         ZclAttribute attribute = cluster.getAttribute(attributeId);
+        int repeat = args.length == 5 ? parseInteger(args[4]) : 10;
 
         if(!cluster.discoverAttributes(false, null).get()) {
             out.println("Failed to discover attributes");
         }
 
-        for(int i = 1; i <= 10; ++i) {
+        for(int i = 1; i <= repeat; ++i) {
             out.println("[ROUND " + i + "] Reading endpoint " + endpoint.getEndpointAddress() + ", cluster " + printCluster(cluster) + ", attribute " + attribute.getId());
             cluster.readAttribute(attribute.getId());
         }
