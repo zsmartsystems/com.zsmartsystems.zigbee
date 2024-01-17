9f315fbcc02a17af6c2afc2898841f0bd7c0dd4d
Patrick Decat <pdecat@gmail.com>
2024-01-17 19:40:23 +0100

    feat: Bump EZSP to V13 (#1413)
    * feat: Bump EZSP to V13
    
    * fix(ci): remove settings.gradle to resolve CI issues
    
    * docs: remove Gradle build instructions from README

--- com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/EzspFrame.java
+++ com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ezsp/EzspFrame.java
@@ -58,7 +58,7 @@ public abstract class EzspFrame {
     /**
      * The maximum supported version of EZSP
      */
-    public static final int EZSP_MAX_VERSION = 12;
+    public static final int EZSP_MAX_VERSION = 13;
 
     /**
      * The network ID bit shift
