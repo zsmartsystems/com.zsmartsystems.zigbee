55280947688890eed98c9eed05d89f97bb7564f1
Chris Jackson <chris@cd-jackson.com>
2024-11-20 15:04:04 +1300

    Improve command line interpretation of endpoint ID (#1448)
    * Improve command line interpretation of endpoint ID
    
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- com.zsmartsystems.zigbee.console/src/main/java/com/zsmartsystems/zigbee/console/ZigBeeConsoleReportingConfigCommand.java
+++ com.zsmartsystems.zigbee.console/src/main/java/com/zsmartsystems/zigbee/console/ZigBeeConsoleReportingConfigCommand.java
@@ -70,15 +70,17 @@ public class ZigBeeConsoleReportingConfigCommand extends ZigBeeConsoleAbstractCo
         if (result.isSuccess()) {
             final ReadReportingConfigurationResponse response = result.getResponse();
             final AttributeReportingStatusRecord attributeReportingStatusRecord = response.getRecords().get(0);
-            
+
             final ZclStatus statusCode = attributeReportingStatusRecord.getStatus();
             if (statusCode == ZclStatus.SUCCESS) {
                 out.println("Cluster " + response.getClusterId() + ", Attribute "
                         + attributeReportingStatusRecord.getAttributeIdentifier() + ", type "
                         + attributeReportingStatusRecord.getAttributeDataType() + ", minimum reporting interval: "
-                        + attributeReportingStatusRecord.getMinimumReportingInterval() + ", maximum reporting interval: "
-                        + attributeReportingStatusRecord.getMaximumReportingInterval() + ", timeout: " 
-                        + attributeReportingStatusRecord.getTimeoutPeriod());
+                        + attributeReportingStatusRecord.getMinimumReportingInterval()
+                        + ", maximum reporting interval: "
+                        + attributeReportingStatusRecord.getMaximumReportingInterval() + ", timeout: "
+                        + (attributeReportingStatusRecord.getTimeoutPeriod() == 0 ? "N/A"
+                                : attributeReportingStatusRecord.getTimeoutPeriod()));
             } else {
                 out.println("Attribute value read error: " + statusCode);
             }
